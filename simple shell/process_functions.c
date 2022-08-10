228140b5b3fc3e878717a32cd686de2b7d91438a
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-08-10 19:13:10 +0300

    f

--- simple shell/process_functions.c
+++ simple shell/process_functions.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * check_path - check if argumnt is already a path
  * @argm: entry argument
@@ -28,10 +28,30 @@ char *execute_command(char *argm)
 	if (buffer != NULL)
 	{
 		free(path),
-		free_list(head);
+			free_list(head);
 		return (buffer);
 	}
 	free(path);
 	free_list(head);
 	return (NULL);
+}
+int _getchar(void)
+{
+	static unsigned char buff[BUFF_SIZE];
+	static int index;
+	static int count;
+
+	index = 0;
+	count = 0;
+	if (index >= count)
+	{
+		index = 0;
+		count = read(STDIN_FILENO, buff, BUFF_SIZE);
+		printf("%i\n", count);
+		if (count == 0)
+			return (EOF);
+		if (count < 0)
+			return (EOF);
+	}
+	return (buff[index++]);
 }
